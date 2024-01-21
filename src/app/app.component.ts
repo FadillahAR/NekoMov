57c3412783b4d9c3f408eff368bbf8104b11be27
FadillahAR <fadillahauliarahman02@gmail.com>
2024-01-21 17:35:26 +0700

    Remove unused file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,10 @@ import { LocalStorageService } from './core/services/local-storage.service';
 export class AppComponent implements OnDestroy{
   destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(private authService: AuthenticationService, private localService: LocalStorageService) {
-    this.authService.getRequestToken().pipe(takeUntil(this.destroy$)).subscribe((resp: any) => {
-      this.authService.approvalToken(resp.request_token);
-    });
+   
   }
 
   ngOnDestroy(): void {
-    this.authService.deleteSession(this.localService.getItem('session-id') as string).subscribe();
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
   }
--- src/app/shared/components/list-movie/list-movie.component.ts
+++ src/app/shared/components/list-movie/list-movie.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { DetailMovie } from 'src/app/core/interfaces/movies.interface';
 
 @Component({
   selector: 'app-list-movie',
   templateUrl: './list-movie.component.html',
-  styleUrls: ['./list-movie.component.scss']
+  styleUrls: ['./list-movie.component.scss'],
 })
 export class ListMovieComponent {
   @Input() listMovie: DetailMovie[] = [];
 
-  constructor() {
-    console.log(this.listMovie)
+  constructor(private router: Router) {
+  }
+
+  gotoDetail(id: number) {
+    this.router.navigate(['movies', 'detail', id]);
   }
 }
